fa1555a7ddc88dd2a7508cee5a8cab84d0275992
Zoli97 <zzoli97@yahoo.com>
2021-05-18 10:23:21 +0300

    this is my landinng page

---
+++ js/fullpage.js
@@ -0,0 +1,9 @@
+var myfullpage = new fullpage("#fullpage",{
+
+    autoScrolling: true,
+    navigation: true,
+    navigationTooltips: ['First', 'Second','Third'],
+    scrollingSpeed: 1500,
+    fitToSection: true,
+
+});
